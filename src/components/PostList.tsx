29a219eae70d688a19c18325359d28b53ecf53cb
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2026-02-04 11:32:07 +0100

    useeffect med fetch finished

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -8,11 +8,16 @@ interface Post {
 }
 
 export default function PostList() {
-    const [posts, setPosts] = useState<Post[]>([])
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null)
 
     useEffect(() => {
       async function getPosts() {
         try {
+            setLoading(true);
+            
+            setError(null);
             const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
             if(!response.ok) {
                 throw new Error("Något gick fel");
@@ -21,20 +26,36 @@ export default function PostList() {
             setPosts(data);
 
         } catch(error) {
-
+            setError("Fel: Kunde inte hämta inlägg");
+        } finally {
+            setLoading(false);
         }
       }
       getPosts();
     }, []);
 
+    // useEffect(() => {
+
+    //     // Den deklarade funktionen getPost
+
+    //     // Funktion som ska köras vid mount eller när en dependencie i arrayen ändras
+    //     //getPosts()
+
+    // }, [])
 
 
     return (
         <section>
                 <h2>Posts</h2>
-                {posts.map((post) => (
-                    <li>{post.title}</li>
-                ))}
+
+                {loading && <p className="text-gray-500">Laddar...</p>}
+                {error && <p className="text-red-500"> Error ... </p>}
+
+                <ul>
+                    {!loading && !error &&posts.map((post) => (
+                        <li>{post.title}</li>
+                    ))}
+                </ul>
         </section>
     )
 
